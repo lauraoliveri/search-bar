bda2b603fb78684d69c45ae9db3fc4c84d847efa
lauraoliveri <lauraoliveri2003@gmail.com>
2025-02-26 13:27:43 +0100

    Add components

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,31 @@
 <script>
-/* 
-  Per importare ed utilizzare un componente dentro un altro devo SEMPRE seguire questi 3 passi:
-  1) Importazione del componente
-  2) Dichiarazione del componente
-  3) Utilizzo del componente
-*/
-// 1) Importazione del componente
 import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 
 export default {
   data() {
     return { 
-      count: 0
+    
     }
   },
-  // 2) Dichiarazione del componente
   components: {
-    AppHeader
+    AppHeader,
+    AppMain
   },
   methods: {
-    incrementCount() {
-      this.count++;
-    }
+    
   }
 }
 </script>
 
 <template>
-  <div>
-    <!-- 3) Utilizzo del componente -->
-    <AppHeader />
-    
-    <main>
-      <button class="btn btn-primary" @click="incrementCount()">
-        {{ count }}
-      </button>
-    </main>
+  <div class="container">
+    <div class="card">
+      <div class="card-body">
+        <AppHeader />
+        <AppMain />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -43,4 +33,13 @@ export default {
 @use 'assets/scss/main' as *;
 // Import all of Bootstrap's CSS
 @import "bootstrap/scss/bootstrap";
+
+.container {
+  padding: 50px;
+}
+
+.card {
+  margin: 0 auto;
+  width: 50%;
+}
 </style>
